5603d946c77c39f3c6823fe33e11030c8c591673
marimonto <marimontod@gmail.com>
2021-04-21 14:58:59 -0500

    the To Do list component

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+  todoList = [
+    { title: 'install NodeJS' },
+    { title: 'install Angular CLI' },
+    { title: 'create new app' },
+    { title: 'serve app' },
+    { title: 'develop app' },
+    { title: 'deploy app' },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
